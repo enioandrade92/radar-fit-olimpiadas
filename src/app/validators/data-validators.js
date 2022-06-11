e5455d3496f726a0ab5dd84e68b1fe32b9ad029d
enioandrade <enio.goulart@gmail.com>
2022-06-10 21:23:00 -0300

    feat: joi validator implemented

---
+++ src/app/validators/data-validators.js
@@ -0,0 +1,40 @@
+const Joi = require('joi');
+const errorGenerator = require('../util/error-generator');
+
+const competitionSchema = Joi.object({
+  name: Joi.string().valid(
+    'competicao de hidratacao',
+    'competicao de yoga',
+    'competicao de perda de peso',
+  ),
+});
+
+const athleteResultSchema = Joi.object({
+  competition: Joi.string().valid(
+    'competicao de hidratacao',
+    'competicao de yoga',
+    'competicao de perda de peso',
+  ).required(),
+  athlete: Joi.string().required(),
+  value: Joi.string.required(),
+  measure: Joi.string.required(),
+  competitionId: Joi.number().positive().required(),
+});
+
+module.exports = {
+  async competition(body) {
+    try {
+      await competitionSchema.validateAsync(body);
+    } catch (error) {
+      errorGenerator('bad_request', error.message);
+    }
+  },
+
+  async athleteResult(body) {
+    try {
+      await athleteResultSchema.validateAsync(body);
+    } catch (error) {
+      errorGenerator('bad_request', error.message);
+    }
+  },
+};
