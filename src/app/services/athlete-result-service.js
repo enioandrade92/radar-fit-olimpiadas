5558a82840517c89a5a49fbc8a992a236ccbc77c
enioandrade <enio.goulart@gmail.com>
2022-06-10 23:04:34 -0300

    feat: Functions 'checkMeasure' and 'checkCompetitionStatus' implemented

--- src/app/services/athlete-result-service.js
+++ src/app/services/athlete-result-service.js
@@ -1,8 +1,54 @@
 const athleteResultModel = require('../models/athlete-result-model');
+const competitionModel = require('../models/competition-model');
+const errorGenerator = require('../util/error-generator');
+
+async function checkCompetitionStatus(id) {
+  const competition = await competitionModel.findById(id);
+  if (!competition) {
+    return errorGenerator('bad_request', 'Competition not found');
+  }
+  if (competition.status === 'closed') {
+    return errorGenerator('bad_request', 'Competition closed');
+  }
+  return true;
+}
+
+function checkMeasure(data) {
+  switch (data.measure) {
+    case 'ml':
+      return {
+        ...data,
+        value: +data.value / 1000,
+        measure: 'l',
+      };
+
+    case 's':
+      return {
+        ...data,
+        value: +data.value / 60,
+        measure: 'm',
+      };
+
+    case 'h':
+      return {
+        ...data,
+        value: +data.value * 60,
+        measure: 'm',
+      };
+
+    default:
+      return {
+        ...data,
+        value: +data.value,
+      };
+  }
+}
 
 module.exports = {
   async create(dataAthleteResult) {
-    const athleteResult = await athleteResultModel.create(dataAthleteResult);
+    await checkCompetitionStatus(dataAthleteResult.competitionId);
+    const processedData = checkMeasure(dataAthleteResult);
+    const athleteResult = await athleteResultModel.create(processedData);
     return athleteResult;
   },
 };
