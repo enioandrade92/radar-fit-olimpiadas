eae677860a652b8dd8238d70890afe293ec98e2f
enioandrade <enio.goulart@gmail.com>
2022-06-10 23:43:34 -0300

    feat: getCompetition validator implemented

--- src/app/validators/data-validators.js
+++ src/app/validators/data-validators.js
@@ -1,31 +1,18 @@
-const Joi = require('joi');
 const errorGenerator = require('../util/error-generator');
-
-const competitionSchema = Joi.object({
-  name: Joi.string().valid(
-    'competicao de hidratacao',
-    'competicao de yoga',
-    'competicao de perda de peso',
-  ),
-});
-
-const athleteResultSchema = Joi.object({
-  competition: Joi.string().valid(
-    'competicao de hidratacao',
-    'competicao de yoga',
-    'competicao de perda de peso',
-    'competicao de lançamento de dardos',
-  ).required(),
-  athlete: Joi.string().required(),
-  value: Joi.string().required(),
-  measure: Joi.string().required(),
-  competitionId: Joi.number().positive().required(),
-});
+const { competitionSchema, athleteResultSchema, getCompetitionSchema } = require('./schemas');
 
 module.exports = {
   async competition(body) {
     try {
       await competitionSchema.validateAsync(body);
+    } catch (error) {
+      errorGenerator('bad_request', error.message);
+    }
+  },
+
+  async getCompetition(body) {
+    try {
+      await getCompetitionSchema.validateAsync(body);
     } catch (error) {
       errorGenerator('bad_request', error.message);
     }
