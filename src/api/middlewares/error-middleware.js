59bc7c6c05e9bb9506458192eb88d29b078126f3
enioandrade <enio.goulart@gmail.com>
2022-06-12 01:47:39 -0300

    refactor: remove console.log in error middleware

--- src/api/middlewares/error-middleware.js
+++ src/api/middlewares/error-middleware.js
@@ -3,12 +3,10 @@ module.exports = (err, _req, res, next) => {
 
   switch (name) {
     case 'bad_request':
-      console.log(err);
       res.status(400).json({ message });
       break;
 
     default:
-      console.log(err);
       res.status(500).json({ message });
       break;
   }
